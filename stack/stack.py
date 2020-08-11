e0736a5fd8e6d3854348b7dcca5166c59f381c08
unknown <mjschuette4@gmail.com>
2020-08-11 15:39:01 -0500

    UHHU

--- stack/stack.py
+++ stack/stack.py
@@ -4,22 +4,47 @@ return elements in Last In First Out order.
 
 1. Implement the Stack class using an array as the underlying storage structure.
    Make sure the Stack tests pass.
+"""
+"""
 2. Re-implement the Stack class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Stack tests pass.
+"""
+"""
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#         def __init__(self):
+#             self.storage = []
+
+#         def __len__(self):
+#            return len(self.storage)
+
+#         def push(self, value):
+#            return self.storage.insert(value)
+
+#         def pop(self):
+#            if len(self.storage) == 0:
+#                return None
+#            return self.storage.pop()
+from .singly_linked_list import LinkedList
+print(LinkedList)
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        return self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size == 0:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
